1342159db6b2c2b33683e5f7d0a0597827206267
Ynryo <ewenatideklein@gmail.com>
2023-12-15 08:54:35 +0100

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,20 @@
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
 
-const char* ssid = "TP-Link_FC6C";
-const char* password = "03798966";
+const char *ssid = "TP-Link_FC6C";
+const char *password = "03798966";
 
 const int led = 2;
 const int capteurLuminosite = 34;
 
 AsyncWebServer server(80);
 
-void setup() {
+void setup()
+{
     //--------------------Serial
     Serial.begin(115200);
-    while(!Serial){
+    while (!Serial)
+    {
         Serial.println("\n");
     }
 
@@ -21,12 +23,40 @@ void setup() {
     pinMode(led, OUTPUT);
     digitalWrite(led, LOW);
     pinMode(capteurLuminosite, INPUT);
-    
+
     //--------------------SPIFFS
-    if(!SPIFFS.begin(true)) {
+    if (!SPIFFS.begin(true))
+    {
         Serial.println("Erreur SPIFFS");
         return;
     }
+    else
+    {
+        Serial.println("SPIFFS loaded");
+    }
+
+    File file = SPIFFS.open("/test.txt", FILE_WRITE);
+
+    if (!file)
+    {
+        Serial.println("There was an error opening the file for writing");
+        return;
+    }
+
+    if (file.print("TEST"))
+    {
+        Serial.println("File was written");
+    }
+    else
+    {
+        Serial.println("File write failed");
+    }
+
+    file.close();
+
+    Serial.println(SPIFFS.exists("/test.txt"));
+    Serial.println(SPIFFS.exists("/index.html"));
+    Serial.println(SPIFFS.exists("/nonexisting.txt"));
 
     // File root = SPIFFS.open("/");
     // File file = root.openNextFile();
@@ -40,56 +70,43 @@ void setup() {
 
     //--------------------WIFI
     WiFi.begin(ssid, password);
-	Serial.print("Tentative de connexion...");
-	
-	while(WiFi.status() != WL_CONNECTED)
-	{
-		Serial.print(".");
-		delay(100);
-	}
-	
-	Serial.println("\n");
-	Serial.println("Connexion etablie!");
-	Serial.print("Adresse IP: ");
-	Serial.println(WiFi.localIP());
+    Serial.print("Tentative de connexion...");
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(100);
+    }
+
+    Serial.println("\n");
+    Serial.println("Connexion etablie!");
+    Serial.print("Adresse IP: ");
+    Serial.println(WiFi.localIP());
 
     //--------------------SERVER
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(SPIFFS, "/index.html", "text/html");
-    });
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(SPIFFS, "/index.html", "text/html"); });
 
-    server.on("/w3.css", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(SPIFFS, "/w3.css", "text/css");
-    });
+    server.on("/w3.css", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(SPIFFS, "/w3.css", "text/css"); });
 
-    server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(SPIFFS, "/script.js", "text/javascript");
-    });
+    server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request)
+              { request->send(SPIFFS, "/script.js", "text/javascript"); });
 
-    server.on("/getDatas", HTTP_GET, [](AsyncWebServerRequest *request){
+    server.on("/getDatas", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
         String datas = String(analogRead(capteurLuminosite));
-        request->send(200, "text/plain", datas);
-    });
+        request->send(200, "text/plain", datas); });
 
-    server.on("/getDatas", HTTP_GET, [](AsyncWebServerRequest *request){
+    server.on("/getDatas", HTTP_GET, [](AsyncWebServerRequest *request)
+              {
         String datas = String(analogRead(capteurLuminosite));
-        request->send(200, "text/plain", datas);
-    });
-
-    // server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
-    //     digitalWrite(led, HIGH);
-    //     request->send(200);
-    // });
-
-    // server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
-    //     digitalWrite(led, LOW);
-    //     request->send(200);
-    // });
+        request->send(200, "text/plain", datas); });
 
     server.begin();
     Serial.println("Serveur activé");
 }
 
-void loop() {
-
+void loop()
+{
 }
