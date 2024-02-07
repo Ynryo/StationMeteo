754f359d7db88294ac4349f04d0855cebb62b91b
Ynryo <ewenatideklein@gmail.com>
2024-02-07 18:22:27 +0100

    commit

--- specstech_datas/post-datas.php
+++ specstech_datas/post-datas.php
@@ -3,8 +3,8 @@ include('connection.php');
 header("Access-Control-Allow-Origin: *");
 if (isset($_POST["temperature"])) {
     $temperature = strip_tags($_POST["temperature"]);
-    $wind_speed = strip_tags($_POST["wind_speed"]);
-    $wind_direction = strip_tags($_POST["wind_direction"]);
+    $wind_speed = strip_tags($_POST["windSpeed"]);
+    $wind_direction = strip_tags($_POST["windDirectionInt"]);
     $humidity = strip_tags($_POST["humidity"]);
     $uv_index = strip_tags($_POST["uvIndex"]);
     $date = date('Y-m-d');
